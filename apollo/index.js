5481918dfb74c24c5c419cc4a675b7de935ed4f2
Tien Pham <r3dsky02@yahoo.com.vn>
2018-05-13 10:20:11 +0300

    [apollo] Use up .env config

--- apollo/index.js
+++ apollo/index.js
@@ -1,5 +1,6 @@
 import 'cross-fetch/polyfill';
-import ApolloClient from "apollo-boost";
+import ApolloClient, { gql } from 'apollo-boost';
+require('dotenv').config()
 
 const client = new ApolloClient({
   uri: "https://api.github.com/graphql",
@@ -11,3 +12,18 @@ const client = new ApolloClient({
     });
   }
 });
+
+const GET_ORGANIZATION = gql`
+  {
+    organization(login: "the-road-to-learn-react") {
+      name
+      url
+    }
+  }
+`;
+
+client
+  .query({
+    query: GET_ORGANIZATION,
+  })
+  .then(console.log);
