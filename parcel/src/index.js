03a3da2367fb4ae1b103ade19f8a21ee6cfcd9a4
Tien Pham <r3dsky02@yahoo.com.vn>
2018-05-18 22:11:18 +0300

    [parcel] Added react

--- parcel/src/index.js
+++ parcel/src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello Parcel Project');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// CAN ALSO IMPORT FROM OTHER PLACES, PARCEL WILL TAKE CARE OF RESOLVING
+// import App from './App';
+// import logo from './logo.svg';
+// import './index.css';
+
+ReactDOM.render(
+  <h1>Hello Parcel React Project</h1>,
+  document.getElementById('root')
+);
