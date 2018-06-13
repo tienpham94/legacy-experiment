b6ff34a1f5268da39a73c53ff949381effcb3879
Tien Pham <r3dsky02@yahoo.com.vn>
2018-06-13 23:16:15 +0300

    [downshift] Functionalities are there

--- downshift/Notes.md
+++ downshift/Notes.md
@@ -1,4 +1,5 @@
 Originally like this but get downshift props get destructured
+
 ```
 <Downshift>
   {(downshift) => (
@@ -10,6 +11,24 @@ Originally like this but get downshift props get destructured
 </Downshift>
 ```
 
-For accessibility purposes we will getLabelProps and getInputProps..., apply some aria- handy things out of the box using props getters
+For accessibility purposes we will getLabelProps and getInputProps..., apply some aria- handy things out of the box using props getters. getMenuProps also
 
 Menu better to use ul instead of div for accessibility purposes
+
+```
+isOpen state from downshift can be taken by destructuring isOpen
+```
+
+Fix itemToString error by providing our own itemToString
+
+```
+ <Downshift itemToString={itemToString}>
+```
+
+Instead of wiring this onClick selectItem we can instead do getItemProps, it requires argument item and can pass down key prop in a funky way
+
+```
+{...getItemProps({ item, key: item.id })}
+```
+
+Cant visually see the highlighted item => Need `highlightedIndex`
--- downshift/src/index.js
+++ downshift/src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom'
 import Downshift from 'downshift'
 import { all as starWarsNames } from 'starwars-names'
 
-console.log(starWarsNames);
+const items = starWarsNames.map(name => ({
+  value: name,
+  id: name.toLowerCase()
+}))
+
+const itemToString = item => (item ? item.value : '')
 
 class App extends React.Component {
   render() {
@@ -11,12 +16,37 @@ class App extends React.Component {
       <div>
         <h1>Autocomplete rocks!</h1>
         <div>
-          <Downshift>
-            {({ getLabelProps, getInputProps }) => (
+          <Downshift itemToString={itemToString}>
+            {({
+              getLabelProps,
+              getInputProps,
+              getItemProps,
+              getToggleButtonProps,
+              isOpen,
+              highlightedIndex
+            }) => (
               <div>
                 <label {...getLabelProps()}>Select a Star Wars character</label>
                 <input {...getInputProps()} />
-                <ul />
+                <button {...getToggleButtonProps()}></button>
+                <ul>
+                  {isOpen
+                    ? items.map((item, index) => (
+                        <li
+                          {...getItemProps({
+                            item,
+                            key: item.id,
+                            style: {
+                              backgroundColor:
+                                index === highlightedIndex ? 'gray' : null
+                            }
+                          })}
+                        >
+                          {item.value}
+                        </li>
+                      ))
+                    : null}
+                </ul>
               </div>
             )}
           </Downshift>
