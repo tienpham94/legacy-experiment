2214871d1bdff995ffdfb32d7bbb389398db7e88
Tien Pham <tien.pham@metropolia.fi>
2018-06-20 17:56:11 +0300

    [canvas] Done

--- canvas/src/index.js
+++ canvas/src/index.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react"
 import ReactDOM from "react-dom"
 
-const App = () => <Canvas width={200} height={200} />
+const App = () => (
+  <Canvas
+    width={200}
+    height={200}
+    draw={(canvas, context) => {
+      context.strokeRect(0, 0, canvas.width, canvas.height)
+    }}
+  />
+)
 
 class Canvas extends Component {
+  componentDidMount() {
+    const context = this.canvas.getContext("2d") //Need 2d context to draw
+    this.props.draw(this.canvas, context)
+  }
+
   render() {
     const { width, height } = this.props
     return (
-      <canvas width={width} height={height} style={{ outline: "1px solid" }} />
+      <canvas
+        ref={node => (this.canvas = node)}
+        width={width}
+        height={height}
+        style={{ outline: "1px solid" }}
+      />
     )
   }
 }
