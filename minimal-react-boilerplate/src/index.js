d858833f7200f70f573b0df422fda88a7c4976ba
Tien Pham <r3dsky02@yahoo.com.vn>
2018-06-26 23:15:06 +0300

    [minimal-react-boilerplate] Finish React setup with Babel and Webpack

--- minimal-react-boilerplate/src/index.js
+++ minimal-react-boilerplate/src/index.js
@@ -1,1 +1,11 @@
-console.log('My Minimal React Webpack Babel Setup');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Minimal React Webpack Babel Setups';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
