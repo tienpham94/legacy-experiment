bb4a4b9b2e5cb736621149c003b17caf193242aa
Tien Pham <r3dsky02@yahoo.com.vn>
2018-07-07 15:56:02 +0300

    The CreateLyric Mutation

--- graphql/client/components/LyricCreate.js
+++ graphql/client/components/LyricCreate.js
@@ -1,14 +1,38 @@
 import React, { Component } from "react";
+import gql from 'graphql-tag'
 
 class LyricCreate extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = { content: ''}
+  }
+
+  onSubmit(e){
+    e.preventDefault()
+
+    this.props.mutation()
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmit.bind(this)}>
         <label>Add a Lyric</label>
-        <input />
+        <input value={this.state.content} onChange={e => this.setState({content: e.target.value})} />
       </form>
     );
   }
 }
+
+const mutation = gql` 
+  mutation AddLyricToSong($content: String, $id: ID){
+    addLyricToSong(content: $content, id: $id){
+      id
+      lyrics {
+        content
+      }
+    }
+  }
+`
 
 export default LyricCreate;
