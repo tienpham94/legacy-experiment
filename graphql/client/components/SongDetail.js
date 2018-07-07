b6f086eff66296d9dd5c89c3b1df0889e240949b
Tien Pham <r3dsky02@yahoo.com.vn>
2018-07-07 16:03:33 +0300

    Showing a List of Lyrics

--- graphql/client/components/SongDetail.js
+++ graphql/client/components/SongDetail.js
@@ -4,6 +4,7 @@ import { Link } from "react-router";
 
 import FetchSong from "../queries/FetchSong";
 import LyricCreate from "./LyricCreate";
+import LyricList from "./LyricList";
 
 class SongDetail extends Component {
   render() {
@@ -17,6 +18,7 @@ class SongDetail extends Component {
       <div>
         <Link to="/">Back</Link>
         <h3>{song.title}</h3>
+        <LyricList />
         <LyricCreate songId={this.props.params.id} />
       </div>
     );
