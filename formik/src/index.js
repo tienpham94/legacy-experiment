f59870affcb6092edbe3aa9de2a2d5a52afe3f24
Tien Pham <tien.pham@metropolia.fi>
2018-07-19 18:16:33 +0300

    [formik] Switch to Form and Field

--- formik/src/index.js
+++ formik/src/index.js
@@ -1,37 +1,37 @@
 import React from "react"
 import { render } from "react-dom"
-import { withFormik } from "formik"
+import { withFormik, Form, Field } from "formik"
 import Yup from "yup"
+
+// With Field can remove onChange and values
+// With Form can remove onSubmit=handleSubmit
 
 const App = ({
   values, // contain current values of all inputs
-  handleChange, // don't need to provide custom onChange function
-  handleSubmit // just pass in and done
+  handleChange // don't need to provide custom onChange function
 }) => (
-  <form onSubmit={handleSubmit}>
-    <input
-      type="email"
-      name="email"
-      placeholder="Email"
-      value={values.email}
-      onChange={handleChange}
-    />
-    <input
-      type="password"
-      name="password"
-      placeholder="Password"
-      value={values.password}
-      onChange={handleChange}
-    />
+  <Form>
+    <Field type="email" name="email" placeholder="Email" />
+    <Field type="password" name="password" placeholder="Password" />
+    <label>
+      <Field type="checkbox" name="newsletter" checked={values.newsletter} />
+      Join our newsletter
+    </label>
+    <Field component="select" name="plan">
+      <option value="free">Free</option>
+      <option value="premium">Premium</option>
+    </Field>
     <button type="submit">Submit</button>
-  </form>
+  </Form>
 )
 
 const FormikForm = withFormik({
-  mapPropsToValues({ email, password }) {
+  mapPropsToValues({ email, password, newsletter, plan }) {
     return {
       email: email || "", // Might not need initial data, can be email: ""
-      password: password || ""
+      password: password || "",
+      newsletter: newsletter || false,
+      plan: plan || "premium"
     }
   },
   handleSubmit(values) {
