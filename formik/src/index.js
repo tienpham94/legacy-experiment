99f4ee49ba4d37a9adabee0e3228d8ade99ee044
Tien Pham <tien.pham@metropolia.fi>
2018-07-19 18:54:11 +0300

    [formik] Add validation and formikBag

--- formik/src/index.js
+++ formik/src/index.js
@@ -1,18 +1,29 @@
 import React from "react"
 import { render } from "react-dom"
 import { withFormik, Form, Field } from "formik"
-import Yup from "yup"
+import * as Yup from "yup"
+
+console.log(Yup)
 
 // With Field can remove onChange and values
 // With Form can remove onSubmit=handleSubmit
 
 const App = ({
   values, // contain current values of all inputs
-  handleChange // don't need to provide custom onChange function
+  handleChange, // don't need to provide custom onChange function
+  errors,
+  touched, // Object { email: ...}
+  isSubmitting
 }) => (
   <Form>
-    <Field type="email" name="email" placeholder="Email" />
-    <Field type="password" name="password" placeholder="Password" />
+    <div>
+      {touched.email && errors.email && <p>{errors.email}</p>}
+      <Field type="email" name="email" placeholder="Email" />
+    </div>
+    <div>
+      {touched.password && errors.password && <p>{errors.password}</p>}
+      <Field type="password" name="password" placeholder="Password" />
+    </div>
     <label>
       <Field type="checkbox" name="newsletter" checked={values.newsletter} />
       Join our newsletter
@@ -21,7 +32,9 @@ const App = ({
       <option value="free">Free</option>
       <option value="premium">Premium</option>
     </Field>
-    <button type="submit">Submit</button>
+    <button type="submit" disabled={isSubmitting}>
+      Submit
+    </button>
   </Form>
 )
 
@@ -34,10 +47,27 @@ const FormikForm = withFormik({
       plan: plan || "premium"
     }
   },
-  handleSubmit(values) {
+  // ValidationSchema gives us a set of error for the fields above as validation occur
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email("Email not valid")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(9, "Password must be 9 characters or longer")
+      .required("Password is required")
+  }),
+  handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
+    setTimeout(() => {
+      if (values.email === "tien@mail.tien") {
+        setErrors({ email: "That email is already taken" })
+      } else {
+        resetForm()
+      }
+      setSubmitting(false)
+      console.log(values)
+    }, 2000)
     //current values
-    console.log(values)
   }
 })(App)
 
-render(<FormikForm email="tien@mail.tien" />, document.getElementById("root"))
+render(<FormikForm />, document.getElementById("root"))
